290d35de1065a44fd617bdfa4002f66983618b4e
Hammad Ahmad <ahmadh19@mail.wlu.edu>
2017-09-27 20:05:20 -0400

    added default command line arguments

--- src/edu/wlu/cs/cs325/WebServer.java
+++ src/edu/wlu/cs/cs325/WebServer.java
@@ -16,9 +16,19 @@ public class WebServer {
 	 */
 	public static void main(String[] args) {
 		ServerSocket server;
+		String docRoot;
+		int port;
 		
-		String docRoot = args[0];
-		int port = Integer.parseInt(args[1]); //TODO: set a default port if none specified
+		if(args.length == 0) { // if no command line arguments specified
+			docRoot = "";  //default document root
+			port = 8888; // default port
+		} else if(args.length == 1) { // if only document root specified
+			docRoot = args[0];
+			port = 8888;
+		} else { // if both document root and port specified
+			docRoot = args[0];
+			port = Integer.parseInt(args[1]); 
+		}
 		
 		try {
 			server = new ServerSocket(port, 15);
